f5dcab7fad8df507830f077778911e161968d0f5
AjanitDevs <90819837+AjanitDevs@users.noreply.github.com>
2022-10-22 02:01:02 +0530

    Update App.js

--- reactfirst/src/App.js
+++ reactfirst/src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
-
+import profile from "./nits_hacks_images/download(1).png";
+import email from "./nits_hacks_images/email.jpg";
+import pass from "./nits_hacks_images/download.png"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "main">
+      <div className = "sub-main">
+        <div>
+          <div className="imgs">
+            <div className= "container-image">
+              <img src={profile} alt="profile" className="profile"/>
+
+            </div>
+
+          </div>
+          <div>
+            <h1>Login Page</h1>
+            <div>
+              <img src={email} alt="email" className="email"/>
+              <input type= "text" placeholder="user name" className="name"/>
+            </div>
+            <div className="second-input">
+              <img src={pass} alt="pass" className="email"/>
+              <input type= "text" placeholder="user name" className="name"/>
+            </div>
+           <div className="login-button">
+           <button>Login</button>
+           </div>
+            <p className="link">
+            <a href="#">Forgot password?</a>or<a href="#">Sign Up</a>
+            </p>
+          </div>
+        </div>
+
+      </div>
+
     </div>
   );
 }
